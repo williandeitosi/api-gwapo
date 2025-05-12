3b5fdf24edabb5e8c2bae8f601bbd266137229bc
williandeitosi <williandeitosi@gmail.com>
2025-05-12 20:57:44 -0300

    wip(controller): login

---
+++ src/modules/user/controller/index.ts
@@ -0,0 +1,24 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { adminSchema } from "../schema";
+import type { AdminService } from "../use-case";
+
+export class AdminController {
+  constructor(private service: AdminService) {}
+
+  async login(reques: FastifyRequest, reply: FastifyReply) {
+    try {
+      const result = adminSchema.safeParse(reques.body);
+      if (!result.success) {
+        return reply.code(400).send({ errors: result.error.format() });
+      }
+      const data = result.data;
+      const { email, password } = data;
+
+      await this.service.login({ email, password });
+
+      return reply.code(200).send({ message: "Login successfully!" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
