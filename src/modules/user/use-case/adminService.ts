8e5f5d301642a98bcb5fddc1632f404d7944f861
williandeitosi <williandeitosi@gmail.com>
2025-05-14 22:30:54 -0300

    feat: create method to login

--- src/modules/user/use-case/adminService.ts
+++ src/modules/user/use-case/adminService.ts
@@ -1,12 +1,42 @@
+import bcrypt from "bcrypt";
 import { db } from "../../../../prisma/db";
 import type { TAdmin } from "../schema";
+import type { AuthService } from "./authService";
 
 export class AdminService {
+  constructor(private authService: AuthService) {}
   async login({ email, password }: TAdmin) {
-    console.log({ email, password });
-
-    const loginAdmin = await db.admin.findUnique({
+    const admin = await db.admin.findUnique({
       where: { email },
     });
+
+    if (!admin) {
+      return {
+        success: false,
+        message: "Invalid email or password",
+      };
+    }
+
+    const passwordMatch = await bcrypt.compare(password, admin.password);
+    if (!passwordMatch) {
+      return {
+        success: false,
+        message: "Invalid email or password",
+      };
+    }
+
+    const token = this.authService.generateTokens({
+      id: Number(admin.id),
+      email: admin.email,
+    });
+
+    await this.authService.saveTokens(admin.id, token.refreshToken);
+
+    return {
+      success: true,
+      message: "Login successful",
+      access_token: token.accessToken,
+      refresh_token: token.refreshToken,
+    };
   }
 }
